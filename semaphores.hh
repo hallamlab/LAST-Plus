6b6e2dec4de7ce83b8ecc47abe9dde5ed6819c3d
dongjaek <david.kim9325@gmail.com>
2015-05-12 02:31:50 -0700

    The semaphore files were wonky because of a capitalized letter. Whoops!

--- semaphores.hh
+++ semaphores.hh
@@ -7,7 +7,7 @@
 #ifdef __APPLE__
   typedef sem_t* SEM_T;
   #define SEM_POST(x) sem_post(x)
-  #define SEM_WAIT(x) sem_wait(X)
+  #define SEM_WAIT(x) sem_wait(x)
 #elif __linux 
   typedef sem_t SEM_T;
   #define SEM_POST(x) sem_post(&x)
